1b8361e7579861edfd7a74e76b6a0e4cb92e0a0c
Fireye04 <Fireye04@users.noreply.github.com>
2025-04-24 18:53:23 +0000

    deploy: a7b20e6183bb1f7ed29c3cdd276fc46b67faafc6

--- Kai/source/scripts.js
+++ Kai/source/scripts.js
@@ -5,3 +5,27 @@ config.ui.stowBarInitially = true;
 /* Hide the back / forward buttons
 config.history.controls = false;
 */
+
+const Sellable = {
+	constructor(cost) {
+		this.cost = cost;
+	},
+	buy() {
+		console.log(cost);
+	},
+};
+
+class weapon {
+	damage(damage) {
+		console.log(damage);
+	}
+}
+
+Object.assign(weapon, Sellable);
+
+console.log("gothere");
+
+const gun = new weapon(3);
+gun.damage(9);
+gun.buy();
+
