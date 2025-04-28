512f58ce498884378695230c18b3bc57b0f908d3
Fireye <codekai16@gmail.com>
2025-04-27 19:51:59 -0700

    Allow cleaner instantiation

--- source/scripts.js
+++ source/scripts.js
@@ -43,14 +43,63 @@ const Damageable = (state) => ({
 
 const Weapon = (name, description, cost, damage) => {
 	let state = { name, description, cost, damage };
+	this.name = name || "Default";
+	this.description = description || "Default";
+	this.cost = cost || 100;
+	this.damage = damage || 0;
 
 	return Object.assign(state, Sellable(state), Damageable(state));
+};
+
+Weapon.prototype.clone = function () {
+	// Return a new instance containing our own data.
+	return new Weapon(this.name, this.description, this.cost, this.damage);
+};
+
+Weapon.prototype.toJSON = function () {
+	// Return a code string that will create a new instance containing our
+	// own data.
+	return Serial.createReviver(
+		String.format(
+			"new Weapon({0},{1},{2},{3})",
+			JSON.stringify(this.name),
+			JSON.stringify(this.description),
+			JSON.stringify(this.cost),
+			JSON.stringify(this.damage),
+		),
+	);
 };
 
 const Augment = (name, description, cost, power, health) => {
 	let state = { name, description, cost, power, health };
 
 	return Object.assign(state, Sellable(state), Changeable(state));
+};
+
+Augment.prototype.clone = function () {
+	// Return a new instance containing our own data.
+	return new Augment(
+		this.name,
+		this.description,
+		this.cost,
+		this.power,
+		this.health,
+	);
+};
+
+Augment.prototype.toJSON = function () {
+	// Return a code string that will create a new instance containing our
+	// own data.
+	return Serial.createReviver(
+		String.format(
+			"new Augment({0},{1},{2},{3},{4})",
+			JSON.stringify(this.name),
+			JSON.stringify(this.description),
+			JSON.stringify(this.cost),
+			JSON.stringify(this.power),
+			JSON.stringify(this.health),
+		),
+	);
 };
 
 window.Weapon = (name, description, cost, damage) =>
