c856bd28de698d2f3e379af8e2d337ba1c24f1ed
Fireye <codekai16@gmail.com>
2025-04-27 17:33:34 -0700

    test

--- source/scripts.js
+++ source/scripts.js
@@ -6,41 +6,69 @@ config.ui.stowBarInitially = true;
 config.history.controls = false;
 */
 
-let $You_Power;
-let $You_Damage;
-const $You_Health = 25
-let $Difficulty;
-let $En_Damage;
-let $En_Health;
 
+// function You(Power, Damage, Health) {
+// 	this.Power = $You_Power;
+// 	this.Damage = $You_Damage;
+// 	this.Health = $You_Health;
+// }
+//
+// const you = new You($You_Power, $You_Damage, $You_Health);
+//
+// function Enemy(Power, Damage, Health, Difficulty) {
+// 	this.Damage = $En_Damage;
+// 	this.Health = $En_Health;
+// 	this.Difficulty = $Difficulty;
+// }
 
-function You (Power, Damage, Health) {
-    this.Power= $You_Power;
-    this.Damage= $You_Damage;
-    this.Health= $You_Health;
+function roll(You_Power, Difficulty) {
+	let numRolls = You_Power - Difficulty;
+	if (numRolls <= 0) {
+		if (Math.floor(Math.random() * 10 + 0.5) === 10) {
+			return "success_at_cost";
+		}
+		return "falure";
+	}
+	const rolls = [];
+	for (let i = 0; i < numRolls; i++) {
+		rolls.push(Math.floor(Math.random() * 10 + 0.5));
+	}
+	let max = Math.max(...array1);
+
+	if (max <= 3) {
+		return "falure";
+	} else if (max <= 6) {
+		return "success_at_cost";
+	} else {
+		return "success";
+	}
 }
-
-const you = new You ($You_Power, $You_Damage, $You_Health);
-
-function Enemy (Power, Damage, Health, Difficulty) {
-    this.Damage= $En_Damage;
-    this.Health= $En_Health;
-    this.Difficulty= $Difficulty;
-}
-
 
 // Roll for Damage//
-function Damage (You_Power, Difficulty, You_Health, En_Damage, En_Health, You_Damage){
-    x= You_Power + Math.floor(Math.random() * 10 +.5) - Difficulty
-    if (x <= 3) {
-        You_Health - En_Damage
-    } else if (x >= 7) {
-        En_Health - You_Damage
-    } else {En_Health - You_Damage; You_Health - En_Damage }
+function Damage(
+	You_Power,
+	Difficulty,
+	You_Health,
+	En_Damage,
+	En_Health,
+	You_Damage,
+) {
+	let result = roll(You_Power, Difficulty);
+	if (result === "success") {
+		En_Health -= You_Damage;
+	} else if (result === "success_at_cost") {
+		En_Health -= You_Damage;
+		You_Health -= En_Damage;
+	} else {
+		You_Health -= En_Damage;
+	}
+	return You_Health, En_Health;
 }
-function StartCombat (Difficulty, En_Health, En_Damage){
-    let local= variables();
-
-
-
+function StartCombat(Difficulty, En_Health, En_Damage) {
+	let local = variables();
+	local.health = 99;
+	while (En_Health > 0 && local.health > 0) {}
 }
+
+window.StartCombat(Difficulty,En_Health,En_Damage) = StartCombat(Difficulty, En_Health, En_Damage);
+
