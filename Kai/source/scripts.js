8ce6854fd3d56ed1096ac5f6495a9d90665542ce
Fireye04 <Fireye04@users.noreply.github.com>
2025-04-28 03:07:08 +0000

    deploy: 65b605901b44ef725f7b7b7344e78b3637b41f84

--- Kai/source/scripts.js
+++ Kai/source/scripts.js
@@ -41,74 +41,62 @@ const Damageable = (state) => ({
 	},
 });
 
-function Weapon(name, description, cost, damage) {
-	let state = { name, description, cost, damage };
-	this.name = name || "Default";
-	this.description = description || "Default";
-	this.cost = cost || 100;
-	this.damage = damage || 0;
+window.Weapon = class Weapon {
+	constructor(name, description, cost, damage) {
+		let state = { name, description, cost, damage };
+		this.name = name || "Default";
+		this.description = description || "Default";
+		this.cost = cost || 100;
+		this.damage = damage || 0;
 
-	return Object.assign(state, Sellable(state), Damageable(state));
-}
-
-Weapon.prototype.clone = function () {
-	// Return a new instance containing our own data.
-	return new Weapon(this.name, this.description, this.cost, this.damage);
+		Object.assign(state, Sellable(state), Damageable(state));
+	}
+	clone() {
+		return new Weapon(this.name, this.description, this.cost, this.damage);
+	}
+	toJSON() {
+		return Serial.createReviver(
+			String.format(
+				"new Weapon({0},{1},{2},{3})",
+				JSON.stringify(this.name),
+				JSON.stringify(this.description),
+				JSON.stringify(this.cost),
+				JSON.stringify(this.damage),
+			),
+		);
+	}
 };
 
-Weapon.prototype.toJSON = function () {
-	// Return a code string that will create a new instance containing our
-	// own data.
-	return Serial.createReviver(
-		String.format(
-			"new Weapon({0},{1},{2},{3})",
-			JSON.stringify(this.name),
-			JSON.stringify(this.description),
-			JSON.stringify(this.cost),
-			JSON.stringify(this.damage),
-		),
-	);
+window.Augment = class Augment {
+	constructor(name, description, cost, power, health) {
+		let state = { name, description, cost, power, health };
+		this.name = name || "Default";
+		this.description || "Default";
+		this.cost = cost || 100;
+		this.power = power || 0;
+		this.health = health || 0;
+
+		Object.assign(state, Sellable(state), Changeable(state));
+	}
+	clone() {
+		return new Augment(
+			this.name,
+			this.description,
+			this.cost,
+			this.power,
+			this.health,
+		);
+	}
+	toJSON() {
+		return Serial.createReviver(
+			String.format(
+				"new Augment({0},{1},{2},{3},{4})",
+				JSON.stringify(this.name),
+				JSON.stringify(this.description),
+				JSON.stringify(this.cost),
+				JSON.stringify(this.power),
+				JSON.stringify(this.health),
+			),
+		);
+	}
 };
-
-function Augment(name, description, cost, power, health) {
-	let state = { name, description, cost, power, health };
-	this.name = name || "Default";
-	this.description || "Default";
-	this.cost = cost || 100;
-	this.power = power || 0;
-	this.health = health || 0;
-
-	return Object.assign(state, Sellable(state), Changeable(state));
-}
-
-Augment.prototype.clone = function () {
-	// Return a new instance containing our own data.
-	return new Augment(
-		this.name,
-		this.description,
-		this.cost,
-		this.power,
-		this.health,
-	);
-};
-
-Augment.prototype.toJSON = function () {
-	// Return a code string that will create a new instance containing our
-	// own data.
-	return Serial.createReviver(
-		String.format(
-			"new Augment({0},{1},{2},{3},{4})",
-			JSON.stringify(this.name),
-			JSON.stringify(this.description),
-			JSON.stringify(this.cost),
-			JSON.stringify(this.power),
-			JSON.stringify(this.health),
-		),
-	);
-};
-
-window.Weapon = (name, description, cost, damage) =>
-	Weapon(name, description, cost, damage);
-
-window.Augment = (name, description, cost, power, health) =>
-	Augment(name, description, cost, power, health);
