c338e42b7e4ad25c84c8fd27b6ef911753675416
cryan724 <cryan724@users.noreply.github.com>
2025-04-29 04:09:14 +0000

    deploy: a02cc24b4d83d4eb6c3867d9dd4114a71d11f5a0

--- Caitlin/source/scripts.js
+++ Caitlin/source/scripts.js
@@ -395,3 +395,9 @@ window.Catalog = class Catalog {
 		return total;
 	}
 };
+
+let shop1 = "deadeye"
+let shop2 = "the iron giant"
+let shop3 = "cyberwares"
+let shop4 = "blackmarket guns"
+let shop5 = "forge and flame"
